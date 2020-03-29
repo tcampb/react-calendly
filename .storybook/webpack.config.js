0f0fe41384e73a435462883694128112aad923d4
Tyler Campbell <t.campbell@calendly.com>
2020-03-29 10:29:18 -0400

    update webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,10 @@
 const path = require("path");
 const SRC_PATH = path.join(__dirname, "../src");
-const STORIES_PATH = path.join(__dirname, "../stories");
 
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    include: [SRC_PATH, STORIES_PATH],
+    include: [SRC_PATH],
     use: [
       {
         loader: require.resolve("ts-loader"),
