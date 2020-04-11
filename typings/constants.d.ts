636709651601de32a12c2f7d3f3be270eb9f2165
SmitharyZach <znathanielsmith@gmail.com>
2020-04-11 10:20:26 -0400

    remove typings

--- typings/constants.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const CALENDLY_SCRIPT_SOURCE = "https://assets.calendly.com/assets/external/widget.js";
-export declare const CALENDLY_STYLESHEET_SOURCE = "https://assets.calendly.com/assets/external/widget.css";
--- typings/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { InlineWidget } from "./components/InlineWidget/InlineWidget";
-export { PopupText } from "./components/PopupText/PopupText";
-export { PopupWidget } from "./components/PopupWidget/PopupWidget";
