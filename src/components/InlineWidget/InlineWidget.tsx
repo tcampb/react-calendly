5045968dbb4f3cc769c8330692e0bc5fbbfc2e86
Tyler Campbell <t.campbell@calendly.com>
2020-10-10 09:32:26 -0400

    wait for iframe to be rendered before updating

--- src/components/InlineWidget/InlineWidget.tsx
+++ src/components/InlineWidget/InlineWidget.tsx
@@ -35,22 +35,34 @@ class InlineWidget extends React.Component<Props> {
     super(props);
     this.widgetParentContainerRef = React.createRef<HTMLDivElement>();
     this.destroyInlineWidget = this.destroyInlineWidget.bind(this);
+    this.getChildNodeCount = this.getChildNodeCount.bind(this);
+    this.shouldWidgetUpdate = this.shouldWidgetUpdate.bind(this);
   }
 
   componentDidUpdate(prevProps: Props) {
-    if (
-      prevProps.url !== this.props.url
-      || prevProps.pageSettings !== this.props.pageSettings
-      || prevProps.prefill !== this.props.prefill
-      || prevProps.utm !== this.props.utm
-    ) {
-      this.destroyInlineWidget();
-      window.Calendly.initInlineWidget({
-        url: withPageSettings(this.props.url, this.props.pageSettings),
-        parentElement: this.widgetParentContainerRef.current!,
-        prefill: this.props.prefill,
-        utm: this.props.utm,
-      });
+    const shouldUpdate = this.shouldWidgetUpdate(prevProps);
+    if (shouldUpdate) {
+      if (!this.getChildNodeCount()) {
+        this.calendlyWidgetListener("inserted", () => {
+          this.calendlyWidgetListener("removed", () => {
+            window.Calendly.initInlineWidget({
+              url: withPageSettings(this.props.url, this.props.pageSettings),
+              parentElement: this.widgetParentContainerRef.current!,
+              prefill: this.props.prefill,
+              utm: this.props.utm,
+            });
+          });
+          this.destroyInlineWidget();
+        });
+      } else {
+        this.destroyInlineWidget();
+        window.Calendly.initInlineWidget({
+          url: withPageSettings(this.props.url, this.props.pageSettings),
+          parentElement: this.widgetParentContainerRef.current!,
+          prefill: this.props.prefill,
+          utm: this.props.utm,
+        });
+      }
     }
   }
 
@@ -78,6 +90,48 @@ class InlineWidget extends React.Component<Props> {
 
   private destroyInlineWidget() {
     this.widgetParentContainerRef.current!.innerHTML = "";
+  }
+
+  private getChildNodeCount() {
+    return this.widgetParentContainerRef.current!.childNodes.length;
+  }
+
+  private calendlyWidgetListener(
+    event: "inserted" | "removed",
+    callback: () => void
+  ) {
+    const isInsertedEvent = event === "inserted";
+    const isRemovedEvent = event === "removed";
+    return new MutationObserver((mutationsList, observer) => {
+      if (isInsertedEvent) {
+        const nodesAdded = mutationsList.some(
+          (record) => !!record.addedNodes.length
+        );
+
+        if (nodesAdded) callback();
+      }
+
+      if (isRemovedEvent) {
+        const nodesRemoved = mutationsList.some(
+          (record) => !!record.removedNodes.length
+        );
+
+        if (nodesRemoved) callback();
+      }
+
+      observer.disconnect();
+    }).observe(this.widgetParentContainerRef.current!, {
+      childList: true,
+    });
+  }
+
+  private shouldWidgetUpdate(prevProps: Props) {
+    return (
+      prevProps.url !== this.props.url ||
+      prevProps.pageSettings !== this.props.pageSettings ||
+      prevProps.prefill !== this.props.prefill ||
+      prevProps.utm !== this.props.utm
+    );
   }
 }
 
