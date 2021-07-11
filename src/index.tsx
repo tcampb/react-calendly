a3d0bd9d90f0389160fa07f63dd088c9e8a8e0af
Yuri Yakovlev <hi@mynameisyuri.com>
2021-07-11 16:51:16 +0200

    Make CalendlyEventListener event types accessible from the package

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,22 @@ import InlineWidget from "./components/InlineWidget/InlineWidget";
 import PopupButton from "./components/PopupButton/PopupButton";
 import PopupWidget from "./components/PopupWidget/PopupWidget";
 import CalendlyEventListener from "./components/CalendlyEventListener/CalendlyEventListener";
+import type {
+  DateAndTimeSelectedEvent,
+  EventScheduledEvent,
+  EventTypeViewedEvent,
+  ProfilePageViewedEvent,
+} from "./components/CalendlyEventListener/CalendlyEventListener";
 import { openPopupWidget, closePopupWidget } from './calendly';
 
 export { InlineWidget };
 export { PopupButton };
 export { PopupWidget };
 export { CalendlyEventListener };
+export {
+  DateAndTimeSelectedEvent,
+  EventScheduledEvent,
+  EventTypeViewedEvent,
+  ProfilePageViewedEvent,
+};
 export { openPopupWidget, closePopupWidget };
